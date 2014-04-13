94e1a134a1b0edecbc853ba77cfaa92d6d3ecfb2
Heiko Reese <mail@heiko-reese.de>
2014-04-14 01:35:57 +0200

    First version of dtlsHandshake done.

--- dtls_record.go
+++ dtls_record.go
@@ -31,11 +31,78 @@ const (
 )
 
 type dtlsHandshake struct {
-    handshakeType uint8
+    raw             []byte
+    handshakeType   uint8
+    length          uint32 // 3 bytes
     messageSequence uint16
-    fragmentOffset uint32
-    fragmentLength uint32
-    handshakeData []byte
+    fragmentOffset  uint32
+    fragmentLength  uint32
+    body            []byte
+}
+
+func (m *dtlsHandshake) Equal(i interface{}) bool {
+    m1, ok := i.(dtlsHandshake)
+    if !ok {
+        return false
+    }
+
+    return bytes.Equal(m.raw, m1.raw) &&
+        m.handshakeType == m.handshakeType &&
+        m.length == m.length &&
+        m.messageSequence == m.messageSequence &&
+        m.fragmentOffset == m.fragmentOffset &&
+        m.fragmentLength == m.fragmentLength &&
+        bytes.Equal(m.body, m1.body)
+}
+
+func (m *dtlsHandshake) unmarshal(data []byte) bool {
+    if len(data) < 14 {
+        return false
+    }
+    m.raw = data
+    m.handshakeType = uint8(data[0])
+    m.length = uint32(data[1])<<16 | uint32(data[2])<<8 | uint32(data[3])
+    if uint32(len(data)) != 4 + m.length {
+        return false
+    }
+    m.messageSequence = uint16(data[4])<<8 | uint16(data[5])
+    m.fragmentOffset = uint32(data[6])<<24 | uint32(data[7])<<16 | uint32(data[8])<<8 | uint32(data[9])
+    m.fragmentLength = uint32(data[10])<<24 | uint32(data[11])<<16 | uint32(data[12])<<8 | uint32(data[13])
+    m.body = make([]byte, len(data[14:]))
+    copy(m.body, data[14:])
+
+    return true
+}
+
+func (m *dtlsHandshake) marshal() []byte {
+    if m.raw != nil {
+        return m.raw
+    }
+
+    length := 10 + len(m.body)
+    m.length = uint32(length)
+
+    buf := make([]byte, 4 + length)
+
+    buf[0]  = m.handshakeType
+    buf[1]  = uint8(length >> 16)
+    buf[2]  = uint8(length >> 8)
+    buf[3]  = uint8(length)
+    buf[4]  = uint8(m.messageSequence >> 8)
+    buf[5]  = uint8(m.messageSequence)
+    buf[6]  = uint8(m.fragmentOffset >> 24)
+    buf[7]  = uint8(m.fragmentOffset >> 16)
+    buf[8]  = uint8(m.fragmentOffset >> 8)
+    buf[9]  = uint8(m.fragmentOffset)
+    buf[10] = uint8(m.fragmentLength >> 24)
+    buf[11] = uint8(m.fragmentLength >> 16)
+    buf[12] = uint8(m.fragmentLength >> 8)
+    buf[13] = uint8(m.fragmentLength)
+    copy(buf[14:], m.body)
+
+    m.raw = buf
+
+    return buf
 }
 
 type dtlsClientHelloMsg struct {
@@ -76,11 +143,11 @@ func BuildClientHello(msgseq uint16, version int, extensions [][]byte) (packet, 
 	buf := bytes.Buffer{}
 
     // add handshake type and length
-    buf.Write(ClientHelloHandshakePart1)
+    ///// buf.Write(ClientHelloHandshakePart1)
 
     // add message sequence, fragment offset and fragment length
     buf.Write(Uint16To2Bytes(uint16(msgseq)))
-    buf.Write(ClientHelloHandshakePart2)
+    ///// buf.Write(ClientHelloHandshakePart2)
 
     // add version
     buf.Write(Uint16To2Bytes(uint16(version)))
--- dtls_record_test.go
+++ dtls_record_test.go
@@ -5,7 +5,7 @@ import (
 	_ "encoding/binary"
 	"encoding/hex"
 	"fmt"
-    "net"
+    _ "net"
     "testing"
 )
 
@@ -14,25 +14,28 @@ type U16BytesTest struct {
     result []byte
 }
 
-func TestEmptyDTLSRecord(t *testing.T) {
-	emptyRecord := BuildDTLSRecord(ContentTypeHandshake, DTLSv10, 0, 0, nil)
-    //hexdump := hex.EncodeToString(emptyRecord)
-    //fmt.Println(hexdump)
-    length := len(emptyRecord)
-    if length != 13 {
-        t.Error("Empty record is ", length, " bytes long, expexted 13")
-    }
-}
+func TestHandshake(t *testing.T) {
+    m := new(dtlsHandshake)
+    var u dtlsHandshake
 
-func TestSomeDTLSRecord(t *testing.T) {
-    r1 := BuildDTLSRecord(ContentTypeHandshake, DTLSv10, 23,
-    0xfedcba0987654321, []byte("Hello World!"))
-    //fmt.Println(hex.EncodeToString(r1))
-    refrec := []byte{ 0x16, 0xfe, 0xff, 0x00, 0x17, 0xba, 0x09, 0x87, 0x65,
-    0x43, 0x21, 0x00, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f,
-    0x72, 0x6c, 0x64, 0x21 }
-    if bytes.Compare(r1, refrec) != 0 {
-        t.Error("r1 does not look like the reference")
+    // build dtlsHandshake struct
+    m.handshakeType = HandshakeTypeClientHello
+    m.messageSequence = 0x1234
+    m.fragmentOffset = 0x5678
+    m.fragmentLength = 0x90ab
+    m.body = []byte{0xaa, 0xbb, 0xcc}
+
+    buf := m.marshal()
+
+    if u.unmarshal(buf) == false {
+        t.Errorf("Unable to unmarshal buf")
+        fmt.Println(hex.Dump(buf))
+    }
+
+    if !m.Equal(u) {
+        t.Errorf("m != m.Marshal().Unmarshal()")
+        fmt.Printf("%#v\n", m)
+        fmt.Printf("%#v\n", u)
     }
 }
 
@@ -41,20 +44,20 @@ func TestSomeDTLSRecord(t *testing.T) {
 //    fmt.Println(hex.Dump(buf))
 //}
 
-func TestClientAgainstServer(t *testing.T) {
-    buf, _ := BuildClientHello(0, DTLSv10, [][]byte{ClientHelloHandshakeHeartbeatExt})
-    pbuf   := BuildDTLSRecord(ContentTypeHandshake, DTLSv10, 0, 0, buf)
-
-    conn, err := net.Dial("udp", "localhost:4433"); if err != nil {
-        t.Error(err)
-    }
-    defer conn.Close()
-    
-    fmt.Println(hex.Dump(pbuf))
-    conn.Write(pbuf)
-    
-    // TODO: read and process answer
-}
+//func TestClientAgainstServer(t *testing.T) {
+//    buf, _ := BuildClientHello(0, DTLSv10, [][]byte{ClientHelloHandshakeHeartbeatExt})
+//    pbuf   := BuildDTLSRecord(ContentTypeHandshake, DTLSv10, 0, 0, buf)
+//
+//    conn, err := net.Dial("udp", "localhost:4433"); if err != nil {
+//        t.Error(err)
+//    }
+//    defer conn.Close()
+//    
+//    fmt.Println(hex.Dump(pbuf))
+//    conn.Write(pbuf)
+//    
+//    // TODO: read and process answer
+//}
 
 func TestUint32To3Bytes(t *testing.T) {
     tests := []U16BytesTest {
